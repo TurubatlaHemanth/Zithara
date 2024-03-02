af77ff9afa8d4c71ba6bf57af6c50501414a77b2
TurubatlaHemanth <turubatlahemanth2002@gmail.com>
2024-03-02 11:52:50 +0530

    final commit

--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -3,32 +3,35 @@ import React from 'react';
 const Table = ({ records }) => {
   return (
     <table>
-    <thead>
-      <tr>
-        <th rowSpan={2}>Customer Name</th>
-        <th rowSpan={2}>Age</th>
-        <th rowSpan={2}>Phone</th>
-        <th rowSpan={2}>Location</th>
-        <th colSpan="2">Created At</th> {/* Merge two columns */}
-      </tr>
-      <tr>
-        <th>Date</th> {/* Subcolumn for Date */}
-        <th>Time</th> {/* Subcolumn for Time */}
-      </tr>
-    </thead>
-    <tbody>
-      {records.map(customer => (
-        <tr key={customer.sno}>
-          <td>{customer.customer_name}</td>
-          <td>{customer.age}</td>
-          <td>{customer.phone}</td>
-          <td>{customer.location}</td>
-          <td>{new Date(customer.created_at).toLocaleDateString()}</td> {/* Display Date */}
-          <td>{new Date(customer.created_at).toLocaleTimeString()}</td> {/* Display Time */}
+      <thead>
+        <tr>
+          <th rowSpan={2}>S.No.</th>
+          <th rowSpan={2}>Customer Name</th>
+          <th rowSpan={2}>Age</th>
+          <th rowSpan={2}>Phone</th>
+          <th rowSpan={2}>Location</th>
+          <th colSpan={2} >Created At
+          </th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+        <tr>
+          <th>Date</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {records.map((customer, index) => (
+          <tr key={customer.sno}>
+            <td>{customer.sno}</td>
+            <td>{customer.customer_name}</td>
+            <td>{customer.age}</td>
+            <td>{customer.phone}</td>
+            <td>{customer.location}</td>
+            <td>{new Date(customer.created_at).toLocaleDateString()}</td>
+            <td>{new Date(customer.created_at).toLocaleTimeString()}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
